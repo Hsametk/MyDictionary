c9d1117a3560339c4af91c0343c9bb615abf6f78
halil <halil@SAMETKOTU>
2022-02-16 15:58:46 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class MyDictionary<N,T>
+    {
+        T[] items;
+        N[] numbers;
+
+        public MyDictionary()
+        {
+            items = new T[0];
+            numbers = new N[0];
+        }
+
+        public void Add(T item,N number)
+        {
+            T[] tempArray = items;
+            N[] tempArray2 = numbers;
+
+            items= new T[tempArray.Length+1];
+            numbers= new N[tempArray2.Length+1];
+
+            for(int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            for (int i = 0; i < tempArray2.Length; i++)
+            {   
+                numbers[i] = tempArray2[i];
+
+            }
+
+            items[items.Length - 1] = item;
+            numbers[numbers.Length - 1] = number;
+            
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyDictionary;
+
+MyDictionary<string, int> names = new MyDictionary<string, int>();
+
+names.Add(20,"Samet");
+names.Add(10,"Ali");
